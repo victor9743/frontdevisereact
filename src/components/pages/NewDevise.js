535b04dbe991dd484b35b79c529b005529d56afb
Victor Costa <victorcosta9743@gmail.com>
2023-02-27 21:07:58 -0300

    Update NewDevise.js

--- src/components/pages/NewDevise.js
+++ src/components/pages/NewDevise.js
@@ -7,18 +7,18 @@ function NewDevise() {
     const [processadores, setProcessadores] = useState([])
     const [placasMae, setPlacasMae] = useState([])
     const [memoriasRam, setMemoriasRam] = useState([])
-    const [placasVideo, setPlacaVideo] = useState([])
+    const [placasVideo, setPlacasVideo] = useState([])
 
     // variveis de atribuição
     const [processador, setProcessador] = useState([])
     const [placaMae, setPlacaMae] = useState([])
-    const [memoriaRam, setMemoriaRam] =  useState([])
     const [totalMemoria, setTotalMemoria] = useState(0)
+    const [placaVideo, setPlacaVideo] = useState([])
     const [slots, setSlots] = useState(0)
+    const [salvarPedido, getSalvarPedido] = useState(false)
 
     //  array de memorias
     let memoriaArray = []
-    let somaMemoria = 0
     
     let memoriasList = []
 
@@ -93,19 +93,26 @@ function NewDevise() {
         .catch((err)=> { console.log(err) })
     }
     
-    function placaVideo(e) {
+    function somaMemoriaFunction(e) {
         let somaArray = []
         let chave = e.target.name
+        var soma = 0;
         if (e.target.checked) {
             memoriaArray[chave] = parseInt(e.target.value)
         }
 
         for(let memoria in memoriaArray) {
             somaArray.push(memoriaArray[memoria])
-            console.log(somaArray)
         }
 
         
+        for(var i = 0; i < somaArray.length; i++) {
+            soma += somaArray[i];
+        }
+
+        if (soma > 0) {
+            getAllPlacasVideo()
+        }    
     }
 
     function forSlots() {
@@ -122,7 +129,7 @@ function NewDevise() {
                                     name={`slotMemoria${i + 1}`}
                                     text={`${m} GB`}
                                     value={m}
-                                    funcao={placaVideo}
+                                    funcao={somaMemoriaFunction}
                                 />
                             </div>
                             
@@ -141,9 +148,38 @@ function NewDevise() {
 
     }
 
+    function getAllPlacasVideo() {
+
+
+        fetch('http://localhost:3000/placasVideo'
+        ,{
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'Application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data)=>{
+            console.log(data)
+            setPlacasVideo(data)
+        })
+        .catch((err)=> { console.log(err) })
+    }
+
+    function getPlacaVideo(e) {
+        setPlacaVideo(e.target.value)
+    
+        getSalvarPedido(true)
+    
+    }
+
+    function enviarPedido() {
+        console.log('fim')
+    }
+
     return (
         <div>
-            <h3>New</h3>
+            <h3>Cadastrar pedido</h3>
             
             { processadores.length > 0 && processadores.map((p, key)=>
                 <RadioInput
@@ -168,6 +204,21 @@ function NewDevise() {
             }        
 
             { memoriasRam.length > 0 && forSlots() }
+
+            {placasVideo.length > 0 && placasVideo.map((p, key) => 
+                <RadioInput
+                    name="placaVideo"
+                    value={p.id}
+                    text={p.Produto}
+                    funcao={getPlacaVideo}
+                    key={key}
+                />
+            )}
+            {salvarPedido &&
+                <div>
+                    <button className="btn btn-success" onClick={enviarPedido}>Salvar pedido</button>
+                </div>
+            }
             
         </div>
     )
